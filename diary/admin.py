637910bcbb3b1a6db9139a1326d11bd77cee429a
eduard.gan <eduard.gan@gmail.com>
2018-09-08 01:29:29 +0500

    removed unneeded and undeveloped apps

--- diary/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from diary.models import DiaryPage
-
-
-admin.site.register(DiaryPage)
--- pictures/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from pictures.models import Picture
-
-
-admin.site.register(Picture)
