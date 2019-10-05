d6b7a6fafee2a628ea6dfe5c4724730c3def03b5
Eduard Gan <Eduard.gan@gmail.com>
2019-10-05 10:58:25 +0500

    domain name change

--- novk/settings.py
+++ novk/settings.py
@@ -19,7 +19,7 @@ except KeyError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'novk.ga']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'novk.site']
 INTERNAL_IPS = ('127.0.0.1',)
 
 INSTALLED_APPS = [
